4069364611dacfec473bee924343e1ace989ad77
Brian Muñoz~ <brianmunozjob@gmail.com>
2023-05-28 17:28:06 -0300

    Frontend: Agregar boton guardar y boton para agregar operacion nueva

--- FRONTEND/src/paginas/libros diarios/LibroDiario.jsx
+++ FRONTEND/src/paginas/libros diarios/LibroDiario.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { Button, Spinner } from "react-bootstrap";
+import { Button, Spinner, Alert } from "react-bootstrap";
 import config from "../../config/config";
 import axios from "axios";
 import { todasLasCuentas } from "../../helpers/todasLasCuentas";
@@ -12,11 +12,14 @@ const LibroDiario = () => {
   const navigate = useNavigate();
   const token = document.cookie.replace("token=", "");
   const [datos, setDatos] = useState([]);
-  const [isLoading,setIsLoading] = useState(false);
-  const [nuevaOperacion, setNuevaOperacion] = useState("");
-  
-const obtenerAcientos = ()=>{
-  axios
+  const [isLoading, setIsLoading] = useState(false);
+  const [ultimoIDOperacion, setUltimoIDOperacion] = useState("");
+  const [errorAlGuardar, setErrorAlGuardar] = useState("");
+  const [descripcionOperacion, setDescripcionOperacion] = useState('');
+  const [mensajeOperacion, setMensajeOperacion] = useState('');
+
+  const obtenerAcientos = () => {
+    axios
       .get(config.APIURL_DESARROLLO + "/librodiario/verAcientos/" + ID, {
         headers: {
           autorizacion: token,
@@ -53,13 +56,13 @@ const obtenerAcientos = ()=>{
           arregloAMostrar.push(...cuentasHaber);
           arregloAMostrar.push({ descripcion_operacion });
         }
-
+        setUltimoIDOperacion(arregloAMostrar[arregloAMostrar.length -2].operacion);
         setDatos(arregloAMostrar);
       })
       .catch((err) => console.log(err));
-}
+  };
   useEffect(() => {
-   obtenerAcientos();
+    obtenerAcientos();
   }, []);
   const totalDebe = () => {
     let total = 0;
@@ -88,13 +91,45 @@ const obtenerAcientos = ()=>{
     return total;
   };
   const guardarAcientos = () => {
-    if (localStorage.getItem("Acientos")) {
-      const acientosGuardados = JSON.parse(localStorage.getItem("Acientos"));      
-      const ultimoIDOperacion =
-        !nuevaOperacion && datos.length > 0
-          ? datos[datos.length - 2].operacion
-          : nuevaOperacion;
-      
+    if(localStorage.getItem('Acientos')){
+      const acientosGuardados =
+        localStorage.getItem("Acientos")
+          ? JSON.parse(localStorage.getItem("Acientos"))
+          : [];
+
+      const verificacion =  acientosGuardados.filter(
+        (el) => el.libro_diario === ID
+      );
+      const operacionesGuardadas = datos.filter(
+        (el) =>
+          el.libro_diario && el.operacion === datos[datos.length - 2].operacion
+      );
+      let nuevoArray = [];
+      nuevoArray.push(...operacionesGuardadas);
+      nuevoArray.push(...verificacion);
+      let totalDebe = 0;
+      let totalHaber = 0;
+      nuevoArray.map((element) => {
+        const esDebe = variacionesPatrimoniales.debe.some(
+          (vp) => vp === element.variacion_patrimonial
+        );
+        if (esDebe) {
+          totalDebe += parseInt(element.monto);
+        } else {
+          totalHaber += parseInt(element.monto);
+        }
+      });
+      if (totalDebe !== totalHaber) {
+        setErrorAlGuardar(
+          "Debe balancear la última operacion si desea guardar (debe: " +
+            totalDebe +
+            " - haber: " +
+            totalHaber +
+            ")"
+        );
+        return;
+      }
+
       acientosGuardados.map((element) => {
         if (element.libro_diario === ID) {
           axios
@@ -113,19 +148,72 @@ const obtenerAcientos = ()=>{
                 },
               }
             )
-            .then((res) => {  })
+            .then((res) => {})
             .catch((err) => {
               console.log(err.response.data);
             });
         }
       });
-      const eliminarSubidosDelLS = acientosGuardados.filter(aciento => aciento.libro_diario !== ID )
-      localStorage.setItem('Acientos', JSON.stringify(eliminarSubidosDelLS))
-      obtenerAcientos()
+      const eliminarSubidosDelLS = acientosGuardados.filter(
+        (aciento) => aciento.libro_diario !== ID
+      );
+      localStorage.setItem("Acientos", JSON.stringify(eliminarSubidosDelLS));
+      obtenerAcientos();
       setIsLoading(true);
-      setTimeout(()=>{setIsLoading(false)},2000);
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 2000);
+    }else{
+      setErrorAlGuardar('No tiene acientos para guardar');
+      setTimeout(() => {
+        setErrorAlGuardar('');
+      }, 3000);
+    }
+  };
+
+  const guardarNuevaOperacion = () => {
+    if(!descripcionOperacion){
+      setMensajeOperacion('Debe completar la descripcion de la operacion');
+      return
+    }
+
+      const acientosGuardados =
+        localStorage.getItem("Acientos")
+          ? JSON.parse(localStorage.getItem("Acientos"))
+          : [];
+
+      const existenAcientos =  acientosGuardados.some(
+        (el) => el.libro_diario === ID
+      );
+      if(existenAcientos){
+        setMensajeOperacion('No se puede crear otra operación sin terminar la última operación');
+        return 
+      }
+    
       
-    } 
+          axios
+            .post(
+              config.APIURL_DESARROLLO + "/librodiario/agregarOperacion/" + ID,
+              {                
+                descripcion: descripcionOperacion,
+libro_diario: ID
+              },
+              {
+                headers: {
+                  autorizacion: token,
+                },
+              }
+            )
+            .then((res) => { 
+              setUltimoIDOperacion(res.data.id)
+              setMensajeOperacion('Operación creada!')})
+            .catch((err) => {
+              console.log(err.response.data);
+            });
+
+    
+    
+   
   };
   return (
     <div>
@@ -154,11 +242,35 @@ const obtenerAcientos = ()=>{
           </select>{" "}
           <Button variant="success">Ver Mayor</Button>
         </div>
-        <Button variant="danger" className="mt-3 mb-3">
-          Ver errores de balance
+        <Button
+          variant="outline-dark"
+          className="mt-3 mb-3"
+          onClick={guardarAcientos}
+        >
+          {isLoading ? (
+            <>
+              {" "}
+              <Spinner
+                as="span"
+                animation="grow"
+                size="sm"
+                role="status"
+                aria-hidden="true"
+              />
+              Guardando...{" "}
+            </>
+          ) : (
+            <img
+              width="20"
+              height="20"
+              src="https://img.icons8.com/color/48/save--v1.png"
+              alt="save--v1"
+            />
+          )}{" "}
+          Guardar últimos cambios
         </Button>
       </div>
-
+      {errorAlGuardar && <Alert variant="danger">{errorAlGuardar}</Alert>}
       <div
         style={{
           overflow: "auto",
@@ -167,37 +279,40 @@ const obtenerAcientos = ()=>{
           overflowY: "hidden",
         }}
       >
-       {isLoading ?   <Spinner animation="border" role="status">
-      <span className="visually-hidden">Guardando...</span>
-    </Spinner>: <LDTabla datos={datos} id={ID}></LDTabla>} 
+        {isLoading ? (
+          <Spinner animation="border" role="status">
+            <span className="visually-hidden">Guardando...</span>
+          </Spinner>
+        ) : (
+          <LDTabla datos={datos} id={ID}></LDTabla>
+        )}
 
         <div className="d-flex justify-content-between">
           <p>
             Trabjando en la operacion: id:{" "}
-            {!nuevaOperacion && datos.length > 0
+            {!ultimoIDOperacion && datos.length > 0
               ? datos[datos.length - 2].operacion
-              : nuevaOperacion}
+              : ultimoIDOperacion}
           </p>
+          {mensajeOperacion && mensajeOperacion !== 'Operación creada!' && <Alert variant="danger">{mensajeOperacion}</Alert> }
+          {mensajeOperacion === 'Operación creada!' && <Alert variant="success">{mensajeOperacion}</Alert>}
           <div className="d-flex justify-content-between mb-3">
+
             <input
               type="text"
               placeholder="Ingrese descripción de la operacion"
               className="p-1 border-0 border-bottom border-primary"
+              value={descripcionOperacion}
+              onChange={(e)=>{setDescripcionOperacion(e.target.value)}}
             />
-            <Button style={{ marginLeft: "10px" }}>
+            <Button
+              style={{ marginLeft: "10px" }}
+              onClick={guardarNuevaOperacion}
+            >
               Empezar nueva operacion
             </Button>
           </div>
         </div>
-          <Button variant="outline-dark" onClick={guardarAcientos}>{isLoading ?<> <Spinner
-          as="span"
-          animation="grow"
-          size="sm"
-          role="status"
-          aria-hidden="true"
-        />
-        Guardando... </>: <img  width="48"  height="48"     src="https://img.icons8.com/color/48/save--v1.png" alt="save--v1"/> }</Button>
-
       </div>
     </div>
   );
